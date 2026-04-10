a204ff30391ef568db34fd48a44012678469ea19
pangxiaozi-12 <peixiangzhe461@gmail.com>
2026-04-10 22:32:28 +0900

    添加了demo文件

---
+++ DEMO/We/js/main.js
@@ -0,0 +1,19 @@
+
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                love: {
+                pink: '#FFB6C1',
+                light: '#FFF0F3',
+                deep: '#FF69B4',
+                accent: '#FF85A1'
+            }
+        },
+            fontFamily: {
+            dancing: ['"Dancing Script"', 'cursive'],
+            montserrat: ['Montserrat', 'sans-serif']
+            }
+        }
+    }
+}
